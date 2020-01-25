9978a187d4ce9c3304cc167ba53640427831e063
Matthijs Stichelbaut <matthijs@MatthijMacDS294.home>
2020-01-25 16:54:19 +0100

    hide expo icons

--- src/components/TabBarIcon.js
+++ src/components/TabBarIcon.js
@@ -4,11 +4,12 @@ import Colors from '../constants/Colors';
 
 export default function TabBarIcon(props) {
 	return (
-		<Ionicons
+		<></>
+		/* <Ionicons
 			name={props.name}
 			size={26}
 			style={{ marginBottom: -3 }}
 			color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
-		/>
+		/> */
 	);
 }
